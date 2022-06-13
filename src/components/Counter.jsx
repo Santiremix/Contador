59a72adf1009854077ae3ca7d411eeace813ba96
Santiremix <santiagoguevara118@gmail.com>
2022-06-13 15:24:04 +0200

    Funcionalidad del contador terminada

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,19 +4,23 @@ import {useState} from 'react';
 function Counter() {
 
     const [count, setCount] = useState(0);
-    let num = 1;
+    // let num = 1;
+    const [num, setNum] = useState(1);
+    const aumentar = () => {
+        setNum(Number(document.getElementById('salto').value))
+    }
 
   return (
     <div>
         <h1>Contador</h1>
-        <label> Valor inicial:</label>
+        <label> Valor inicial: </label>
             <input type="number" placeholder='Introduce un número' id='number' />
             <button onClick={() => setCount(Number(document.getElementById('number').value))}>Enviar</button><br></br>
             <label>Indice de salto: </label>
             <input type="number" placeholder='Salto' id='salto'/>
-            <button onClick={() => num = Number(document.getElementById('salto').value)}>Enviar</button>
+            <button onClick={aumentar}>Enviar</button>
         <div>
-            <p>{count}</p>
+            <p><b>{count}</b></p>
             <button onClick={() => setCount(count + num)}>Incrementar</button>
             <button onClick={() => setCount(count - num)}>Decrementar</button>
         </div>
@@ -25,22 +29,3 @@ function Counter() {
 }
 
 export default Counter
-
-
-// export const Counter = () => {
-//     const [count, setCount] = useState(0);
-//   return (
-//     <div>
-//         <h1>Contador</h1>
-//         <label> Valor inicial:
-//             <input type="text" name="name" placeholder='Introduce un número' id='number' />
-//         </label>
-//         <div>
-//             <p>{count}</p>
-//             <button onClick={() => setCount(count + 1)}>Incrementar</button>
-//             <button onClick={() => setCount(count - 1)}>Decrementar</button>
-//         </div>
-//     </div>
-//   )
-// }
-
